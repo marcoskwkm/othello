d5e591a63dde20f18d76df4fa8824d86b01dca17
Marcos Kawakami <marcoskwkm@gmail.com>
2021-04-02 02:41:52 -0300

    Initialize repo with react-ts template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="border m-8 p-8 border-red-400 rounded-md text-center">
+      Hello, world!
+    </div>
+  )
+}
+
+export default App
